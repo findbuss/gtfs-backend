3b10d3c1ff8b489062204431c8fc8d1365d788f2
douglasdans <douglas.dans@gmail.com>
2024-12-03 09:11:32 -0300

    feat: permite busca de rotas por nome longo além do ID

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,17 @@ import { getRoutes } from "gtfs";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const { route_id } = req.query;
-  const routes = getRoutes({ route_id });
-  res.json(routes);
+  const { route_id, route_long_name } = req.query;
+
+  if (route_id) {
+    const routes = getRoutes({ route_id });
+    res.json(routes);
+  }
+
+  if (route_long_name) {
+    const routes = getRoutes({ route_long_name });
+    res.json(routes);
+  }
 });
 
 export default router;
